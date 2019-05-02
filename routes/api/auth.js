24fecca7660c130eced880608e651c6903b8dc75
Daniel Carlyle <dcarlyle@rto-ero.org>
2019-05-02 13:50:25 -0400

    4th commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,88 @@ const express = require("express");
 const router = express.Router();
 //bring in the middleware
 const auth = require("../../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+
+//bring in the User Model
+const User = require("../../models/User");
 
 // @route GET api/auth
 // @description test route
 // @access value Public
 //add middleware as 2nd parameter below
-router.get("/", auth, (req, res) => res.send("Authorization Route"));
+router.get("/", auth, async (req, res) => {
+  //res.send("Authorization Route"));
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST api/auth is the endpoint
+// @description Authenticate user & get token
+// @access value Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //Adding a variable allows you to stop manually adding re.body e.e.g, all the time
+    const { email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      //compare method takes plain text password and encrypted pw and checks them to see if they are the same
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          //MongoDB uses _id but we don't need to worry because they use an abstraction
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
