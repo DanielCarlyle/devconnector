764ebfa73be175e3578d7398f8d3a5d0cbf8b4b7
Daniel Carlyle <dcarlyle@rto-ero.org>
2019-06-13 16:20:48 -0400

    latest edits and additions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -45,6 +45,11 @@ const Register = ({ setAlert, register }) => {
       // }
     }
   };
+
+  //Redirect if looged in
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <Fragment>
@@ -110,11 +115,21 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 //setAlert allows us to access props.setAlert (above in Component)
+///export default connect(
+//null,
+//{ setAlert, register }
+//)(Register);
+
 export default connect(
-  null,
+  mapStateToProps,
   { setAlert, register }
 )(Register);
